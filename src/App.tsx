1625b574a301332787fe01f0d8095d4b6586f79c
CharlyLen <69686011+CharlyLen@users.noreply.github.com>
2023-07-04 15:10:57 -0600

    Se agrega el proyecto al repositorio

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This page is right now in proces
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
